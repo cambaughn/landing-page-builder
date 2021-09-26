c6fa83591412da9cb2a6d3543d573dce8b3e87a0
Cameron Baughn <cam.baughn@gmail.com>
2021-09-26 10:39:24 -1000

    Add sections component

--- components/PageBuilder/PageBuilder.js
+++ components/PageBuilder/PageBuilder.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './PageBuilder.module.scss';
 import Sidebar from '../Sidebar/Sidebar';
-
+import Sections from '../Sections/Sections';
 
 export default function PageBuilder({}) {
   return (
     <div className={styles.container}>
-        <Sidebar />
+      <Sidebar />
+      <Sections />
     </div>
   )
 }
